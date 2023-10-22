bf6c025059a33af85b87f7c1f2494bff2ad3c62c
Luk4sy <302597565@qq.com>
2023-10-22 20:51:35 +0800

    Initial commit

---
+++ season1/01-复杂度/TimeTool.java
@@ -0,0 +1,23 @@
+public class TimeTool {
+    public interface Task {
+        void execute();
+    }
+
+    /**
+     * 测试指定任务的执行时间
+     *
+     * @param task 需要测试的任务
+     */
+    public static void test(String title, Task task) {
+        if (task == null) return;
+        System.out.println(title);
+        System.out.println("开始：" + System.currentTimeMillis());
+        long begin = System.nanoTime();
+        task.execute();
+        long end = System.nanoTime();
+        System.out.println("结束：" + System.currentTimeMillis());
+        double delta = (end - begin) / 1_000_000.0;
+        System.out.println("耗时：" + delta + " ms");
+        System.out.println("--------------------------------");
+    }
+}
