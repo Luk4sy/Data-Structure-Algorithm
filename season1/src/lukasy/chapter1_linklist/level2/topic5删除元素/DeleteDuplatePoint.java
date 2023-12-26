df145a93e50cb12ed375683f770d005323d52ef2
Luk4sy <302597565@qq.com>
2023-12-26 22:02:46 +0800

    2023/12/26

--- season1/src/lukasy/chapter1_linklist/level2/topic5删除元素/DeleteDuplatePoint.java
+++ season1/src/lukasy/chapter1_linklist/level2/topic5删除元素/DeleteDuplatePoint.java
@@ -23,4 +23,31 @@ public class DeleteDuplatePoint {
         }
         return head;
     }
+
+    /**
+     * 重复元素都不要
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) { //判断当前节点下一个节点和下下一个节点的值是否相等
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next; //相等则将相同节点删除，当前节点指向下下一个节点
+                }
+            } else {
+                cur = cur.next; //继续向前遍历
+            }
+        }
+        return dummy.next;
+    }
 }
